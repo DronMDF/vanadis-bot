56d38421c4210099a61ae6648a623f6c02ec964d
Andrey Valyaev <dron.valyaev@gmail.com>
2016-10-26 15:27:43 +0300

    Initial code coverage by #4

--- style.sh
+++ style.sh
@@ -2,4 +2,6 @@
 set -e
 find ./ -name "*.py" | xargs -r pep8 --show-source --max-line-length=100 --ignore=W191,E128
 find ./ -name "*.py" | xargs -r pylint --max-line-length=100 --indent-string="	" -fparseable \
-	-e all -r no
+	-r no --const-rgx="[a-z][a-z0-9_]{0,30}$" --variable-rgx="[a-z][a-z0-9_]{0,30}$" \
+	--function-rgx="[a-z][a-zA-Z0-9]{0,30}$" --module-rgx="[a-zA-Z][a-zA-Z0-9]{0,30}$" \
+	--method-rgx="[a-z][a-zA-Z0-9]{0,30}$" -e all -d missing-docstring
