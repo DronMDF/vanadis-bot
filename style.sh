a5c0a2705d6e75212d4b243ec517a1974e470aa3
Andrey Valyaev <dron.valyaev@gmail.com>
2016-10-26 23:24:12 +0300

    Fix wrong assertion in testIssueLocation.py by #7

--- style.sh
+++ style.sh
@@ -5,4 +5,4 @@ find ./ -name "*.py" | xargs -r pep8 --show-source --max-line-length=100 --ignor
 find ./ -name "*.py" | xargs -r pylint --max-line-length=100 --indent-string="	" -fparseable \
 	-r no --const-rgx="[a-z][a-z0-9_]{0,30}$" --variable-rgx="[a-z][a-z0-9_]{0,30}$" \
 	--function-rgx="[a-z][a-zA-Z0-9]{0,30}$" --module-rgx="[a-zA-Z][a-zA-Z0-9]{0,30}$" \
-	-e all -d missing-docstring,bad-continuation,too-few-public-methods,invalid-name,no-member
+	-e all -d missing-docstring,bad-continuation,too-few-public-methods,invalid-name
--- test/testIssueLocation.py
+++ test/testIssueLocation.py
@@ -8,7 +8,7 @@ class TestIssueLocation(TestCase):
 		a = IssueLocation('a.txt', 123, 321)
 		# Then
 		self.assertEqual(a, IssueLocation('a.txt', 123, 321))
-		self.assertNe(a, IssueLocation('b.txt', 123, 321))
-		self.assertNe(a, IssueLocation('a.txt', 423, 321))
-		self.assertNe(a, IssueLocation('a.txt', 123, 521))
-		self.assertNe(a, IssueLocation('a.txt', 123))
+		self.assertNotEqual(a, IssueLocation('b.txt', 123, 321))
+		self.assertNotEqual(a, IssueLocation('a.txt', 423, 321))
+		self.assertNotEqual(a, IssueLocation('a.txt', 123, 521))
+		self.assertNotEqual(a, IssueLocation('a.txt', 123))
