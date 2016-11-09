a78a74f2737e6a333963ac71b8b7be7524e16c34
Andrey Valyaev <dron.valyaev@gmail.com>
2016-11-09 22:35:19 +0300

    Full xml implementation by #7

--- test/testXml.py
+++ test/testXml.py
@@ -10,3 +10,22 @@ class TestXml(TestCase):
 		text = media.xml()
 		# Then
 		self.assertEqual(text, '<?xml version="1.0"?><root></root>')
+
+	def testPrimitiveTypes(self):
+		media = Xml('primitives')
+		# When
+		media.write(int=12345)
+		media.write(string='test')
+		# Then
+		self.assertIn('<primitives><int>12345</int><string>test</string>', media.xml())
+
+	def testPrintableTypes(self):
+		class Printable:
+			def print(self, media):
+				media.write(printed='yes')
+		media = Xml('primitives')
+		printable = Printable()
+		# When
+		media.write(printable=printable)
+		# Then
+		self.assertIn('<primitives><printable><printed>yes</printed></printa', media.xml())
